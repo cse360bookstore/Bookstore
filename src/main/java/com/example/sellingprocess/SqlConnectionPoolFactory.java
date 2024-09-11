411c775888891e4cfd88a945327f237618c10779
ehof17 <ehof17@gmail.com>
2024-09-11 06:20:08 -0700

    commented out password

--- src/main/java/com/example/sellingprocess/SqlConnectionPoolFactory.java
+++ src/main/java/com/example/sellingprocess/SqlConnectionPoolFactory.java
@@ -17,7 +17,7 @@ public class SqlConnectionPoolFactory{
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(String.format("jdbc:mysql:///%s", DB_NAME));
         config.setUsername(DB_USER);
-        config.setPassword(DB_PASS);
+        //config.setPassword(DB_PASS);
         config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
         config.addDataSourceProperty("cloudSqlInstance", INSTANCE_CONNECTION_NAME);
 
