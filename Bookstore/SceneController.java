ef5e8f8f44bda9298aa6be01c33056967b8a89ae
Gabriel Gonzalez <ggonza90@asu.edu>
2024-09-28 15:02:17 -0700

    Updated to add TransactionView and SceneController scene

--- Bookstore/SceneController.java
+++ Bookstore/SceneController.java
@@ -7,6 +7,8 @@ public class SceneController {
 	
 	private Stage primaryStage;
 	private Scene adminScene;
+	private Scene transactionScene;
+	private Scene statisticsScene;
 	
 	public SceneController(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -14,13 +16,25 @@ public class SceneController {
 	
 	public void loadScene() {
 		AdminDashboard adminDash = new AdminDashboard(this);
+		TransactionView transactionDash = new TransactionView();
+		StatisticsView statisticsDash = new StatisticsView();
 		
 		adminScene = new Scene(adminDash.getRoot(), 800, 850);
+		transactionScene = new Scene(transactionDash.getRoot(), 800, 850);
+		statisticsScene = new Scene(statisticsDash.getRoot(), 800, 850);
 		
 	}
 	
 	public void showAdminScene() {
 		primaryStage.setScene(adminScene);
+	}
+	
+	public void showTransactionScene() {
+		primaryStage.setScene(transactionScene);
+	}
+	
+	public void showStatisticsScene() {
+		primaryStage.setScene(statisticsScene);
 	}
 
 }
