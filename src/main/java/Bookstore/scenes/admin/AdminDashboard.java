992feeccbb7711d0ee5e6a67f5fde0d4e103d08e
ehof17 <ehof17@gmail.com>
2024-10-11 06:55:50 -0700

    rewire admin

--- src/main/java/Bookstore/scenes/admin/AdminDashboard.java
+++ src/main/java/Bookstore/scenes/admin/AdminDashboard.java
@@ -20,15 +20,20 @@ import javafx.stage.FileChooser;
 
 public class AdminDashboard{
 
-	private ImageView userImage;
+	private ImageView imageSelected;
 	private AnchorPane anchorPane;
 	private TransactionView transactionView;
+	private StatisticsView statisticsView;
 	private String name;
 
 	public AdminDashboard() {
+		this.transactionView = new TransactionView();
+		this.statisticsView = new StatisticsView();
 		createUI();
 	}
 	public AdminDashboard(String name) {
+		this.transactionView = new TransactionView();
+		this.statisticsView = new StatisticsView();
 		this.name = name;
 		createUI();
 	}
@@ -38,45 +43,68 @@ public class AdminDashboard{
 	public AnchorPane getRoot() {
 		return anchorPane;
 	}
-	
+
 	private void createUI() {
 		anchorPane = new AnchorPane();
-		
+
 		Rectangle r1 = new Rectangle(300.0f, 80.0f, Color.MAROON);
 	    r1.setArcHeight(10.0d);
 		r1.setArcWidth(10.0d);
-		
-		// Admin Dash 
+
+		// Admin Dash
 		Text adminDash = new Text("Admin Dashboard");
 		adminDash.setFill(Color.WHITE);
 		adminDash.setFont(Font.font("Arial", 28));
-	
+
 		// Stack admin text and rectangle
 		StackPane stack = new StackPane();
 		stack.getChildren().addAll(r1, adminDash);
 		AnchorPane.setTopAnchor(stack, 75.0);
 		AnchorPane.setLeftAnchor(stack, 250.0);
 		anchorPane.getChildren().add(stack);
-		
-		// Transaction Button 
+
+		// Transaction Button
 		Button transactionButton = new Button("Transactions");
 		transactionButton.setTextFill(Color.WHITE);
 		transactionButton.setStyle("-fx-background-color: Maroon");
-		
+
 		// Statistics Button
 		Button statisticsButton = new Button("Statistics");
 		statisticsButton.setTextFill(Color.BLACK);
 		statisticsButton.setStyle("-fx-background-color: #CCCC00");
-	
+
 		// Settings Button
 		Button settingsButton = new Button("Settings");
 		settingsButton.setTextFill(Color.WHITE);
 		settingsButton.setStyle("-fx-background-color: Maroon");
-		
+
+		// Transaction scene
+		transactionButton.setOnAction(event -> {
+			transactionButton.setStyle("-fx-background-color: Maroon");
+			settingsButton.setStyle("-fx-background-color: Gray");
+			statisticsButton.setStyle("-fx-background-color: Gray");
+			displayTransactionView();
+		});
+
+		// Statistics scene
+		statisticsButton.setOnAction(event -> {
+			statisticsButton.setStyle("-fx-background-color: #CCCC00");
+			transactionButton.setStyle("-fx-background-color :Gray");
+			settingsButton.setStyle("-fx-background-color :Gray");
+			displayStatisticsView();
+		});
+
+		// TODO: Create settings scene
+		settingsButton.setOnAction(event -> {
+			settingsButton.setStyle("-fx-background-color: Maroon");
+			transactionButton.setStyle("-fx-background-color: Gray");
+			statisticsButton.setStyle("-fx-background-color: Gray");
+		});
+
 		// Define events for each button
 		transactionButton.setOnAction(event -> displayTransactionView());
-		
-		// Keep buttons organized horizontally 
+
+		// Keep buttons organized horizontally
 		HBox hBox = new HBox(80);
 		hBox.setAlignment(Pos.CENTER);
 		hBox.getChildren().addAll(transactionButton, statisticsButton, settingsButton);
@@ -84,9 +112,9 @@ public class AdminDashboard{
 		AnchorPane.setLeftAnchor(hBox, 200.0);
 		anchorPane.getChildren().add(hBox);
 
-	    // Stack all user info box 	
+	    // Stack all user info box
 		StackPane userInfoStack = new StackPane();
-		
+
 		// Top left user info box
 		Rectangle userInfo = new Rectangle(150.0f, 150.0f, Color.MAROON);
 		userInfo.setArcHeight(10.0d);
@@ -104,32 +132,32 @@ public class AdminDashboard{
 		VBox userInfoBox = new VBox();
 		userInfoBox.setAlignment(Pos.BOTTOM_CENTER);
 		userInfoBox.getChildren().addAll(userName);
-		
-		// Stack user info 
+
+		// Stack user info
 		userInfoStack.setAlignment(Pos.TOP_LEFT);
 		anchorPane.getChildren().add(userInfoStack);
-		
-		// Create user image placeholder 
+
+		// Create user image placeholder
 		Circle userImage = new Circle(50);
 		StackPane.setMargin(userImage, new Insets(0, 0, 20, 0));
 		userImage.setFill(Color.YELLOW);
 		StackPane.setAlignment(userImage, Pos.CENTER);
-	
-		// Add all children objects to stack 
-		
+
+		// Add all children objects to stack
+
 		// Add button to allow user image upload
 		// TODO: Center this button so it's below circle placeholder and above user email
-		Button userImageUpload = new Button("Upload Iamge");
+		Button userImageUpload = new Button("Upload Image");
 		userImageUpload.setOnAction(event -> uploadUserImage(anchorPane));
 		AnchorPane.setTopAnchor(userImageUpload, 200.0);
 		AnchorPane.setLeftAnchor(userImageUpload, 10.0);
-		
-	
+
+
 		userInfoStack.getChildren().addAll(userInfo, userName, userInfoBox, userImage, userImageUpload);
 	}
-	
+
 	// Extend to allow user to choose image from computer to upload for profile picture
-	// TODO: After user selects file to use as profile picture, it fails to display image. 
+	// TODO: User image doesn't upload to profile picture.
 	private void uploadUserImage(AnchorPane anchorPane2) {
 		// Select image file
 		FileChooser chooseImage = new FileChooser();
@@ -137,25 +165,45 @@ public class AdminDashboard{
 		// Specify accepted file type
 		chooseImage.getExtensionFilters().addAll(
 				new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg"));
-		
+
 		File selectedFile = chooseImage.showOpenDialog(anchorPane.getScene().getWindow());
 		if(selectedFile != null) {
-			// Set image file as profile picture 
+			// Set image file as profile picture
 			Image profilePic = new Image(selectedFile.toURI().toString());
-			userImage = new ImageView(profilePic);
+			// was userImage
+			imageSelected = new ImageView(profilePic);
 			// Image dimensions
-			userImage.setFitWidth(40);
-			userImage.setFitHeight(40);
-			userImage.setPreserveRatio(true);
-			
+			imageSelected.setFitWidth(40);
+			imageSelected.setFitHeight(40);
+			imageSelected.setPreserveRatio(true);
+
 		}
-		
+
 	}
-	
+
 	private void displayTransactionView() {
 		transactionView = new TransactionView();
 		AnchorPane transactionPane = transactionView.getRoot();
+		anchorPane.getChildren().removeIf(node -> node instanceof AnchorPane && node != getRoot());
+		AnchorPane.setTopAnchor(transactionPane, 300.0);
+		AnchorPane.setLeftAnchor(transactionPane, 50.0);
+		AnchorPane.setBottomAnchor(transactionPane, 250.0);
+
 		anchorPane.getChildren().add(transactionPane);
+	}
+
+	private void displayStatisticsView() {
+
+		// Define button color change
+
+		anchorPane.getChildren().removeIf(node -> node instanceof AnchorPane && node != getRoot());
+
+		AnchorPane statisticsPane = statisticsView.getRoot();
+		AnchorPane.setTopAnchor(statisticsPane, 300.0);
+		AnchorPane.setLeftAnchor(statisticsPane, 50.0);
+		AnchorPane.setBottomAnchor(statisticsPane, 250.0);
+
+		anchorPane.getChildren().add(statisticsPane);
 	}
 }
 
--- src/main/java/Bookstore/scenes/admin/TransactionView.java
+++ src/main/java/Bookstore/scenes/admin/TransactionView.java
@@ -1,82 +1,91 @@
 package Bookstore.scenes.admin;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class TransactionView {
 	private AnchorPane rootPane;
-	
+
 	public TransactionView() {
 		initializeUI();
 	}
-	
+
 	public AnchorPane getRoot() {
 		return rootPane;
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	private void initializeUI() {
 		rootPane = new AnchorPane();
-	
-		// SHow transaction data
-		TableView<TransactionData> table = new TableView<>();
-		
-		// Username Column 
-		TableColumn<TransactionData, String> usernameColumn = new TableColumn<>("Username");
-		usernameColumn.setMinWidth(150);
+
+		// Show transaction data
+		TableView<CustomerData> dataTable = new TableView<>();
+
+		// Username Column
+		TableColumn<CustomerData, String> usernameColumn = new TableColumn<>("Username");
+		usernameColumn.setMinWidth(230);
 		usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
-		
+
 		// Category Column
-		TableColumn<TransactionData, String> categoryColumn = new TableColumn<>("Category");
-		categoryColumn.setMinWidth(150);
+		TableColumn<CustomerData, String> categoryColumn = new TableColumn<>("Category");
+		categoryColumn.setMinWidth(230);
 		categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
-		
-		// Price Column 
-		TableColumn<TransactionData, Double> priceColumn = new TableColumn<>("Price");
-		priceColumn.setMinWidth(150);
+
+		// Price Column
+		TableColumn<CustomerData, Double> priceColumn = new TableColumn<>("Price");
+		priceColumn.setMinWidth(230);
 		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-		
-		table.getColumns().addAll(usernameColumn, categoryColumn, priceColumn);
-		
+
+		dataTable.getColumns().addAll(usernameColumn, categoryColumn, priceColumn);
+
+
 		// Add code for getting data from database to add to table
-		
-		// Position table in center 
-		//AnchorPane.setTopAnchor(table, 100.0);
-		//AnchorPane.setLeftAnchor(table, 100.0);
-		AnchorPane.setBottomAnchor(table, 100.0);
-		
-		rootPane.getChildren().add(table);
+		//TODO: Sample data doesn't display on table, needs fixing
+		dataTable.getItems().add(new CustomerData("User", "Used Like New", 10.00));
+		dataTable.getItems().add(new CustomerData("User", "Moderately Used", 15.00));
+		dataTable.getItems().add(new CustomerData("User", "Heavily Used", 20.00));
+
+
+		// Position table in center
+		//AnchorPane.setTopAnchor(dataTable, 300.0);
+		//AnchorPane.setLeftAnchor(dataTable, 50.0);
+		//AnchorPane.setBottomAnchor(dataTable, 250.0);
+
+		rootPane.getChildren().add(dataTable);
+
 	}
-	
+
 	// Helper class to hold database data
-	public class TransactionData{
+	public static class CustomerData{
 		private String username;
 		private String category;
 		private Double price;
-		
-		public TransactionData(String Username, String Category, Double Price) {
-			this.username = Username;
-			this.category = Category;
-			this.price = Price;
-			
+
+		public CustomerData(String username, String category, Double price) {
+			this.username = username;
+			this.category = category;
+			this.price = price;
+
 		}
-		
-		// Getters for table 
+
+		// Getters for table
 		public String getUsername() {
 			return username;
 		}
-		
+
 		public String getCategory() {
 			return category;
 		}
-		
+
 		public Double getPrice(){
 			return price;
 		}
-		
+
 	}
 }
-
-
-
